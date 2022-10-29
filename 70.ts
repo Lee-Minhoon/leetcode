5334817e50ed1d86403560c2e036bb09310064c4
lee minhoon <hackids@naver.com>
2022-10-29 21:23:50 +0900

    solved 70, 972

---
+++ 70.ts
@@ -0,0 +1,9 @@
+const hash = new Array(45);
+hash[0] = 1;
+hash[1] = 2;
+hash[2] = 3;
+
+function climbStairs(n: number): number {
+  if (hash[n - 1]) return hash[n - 1];
+  return (hash[n - 1] = climbStairs(n - 1) + climbStairs(n - 2));
+}
