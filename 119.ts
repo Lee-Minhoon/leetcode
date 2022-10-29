fe4d2d96b60d9d6f3324b053dc45103e7a3c1e41
lee minhoon <hackids@naver.com>
2022-10-29 20:51:37 +0900

    solved 119

---
+++ 119.ts
@@ -0,0 +1,11 @@
+function getRow(rowIndex: number): number[] {
+  if (rowIndex === 0) return [1];
+  const current = [];
+  const top = getRow(rowIndex - 1);
+  current.push(1);
+  for (let i = 0; i < top.length - 1; i++) {
+    current.push(top[i] + top[i + 1]);
+  }
+  current.push(1);
+  return current;
+}
