1c370c1d78ef9e9beaa5943825512af05e47355b
Lee Minhoon <hackids@naver.com>
2024-01-15 09:46:23 +0900

    added 162

---
+++ 162.ts
@@ -0,0 +1,13 @@
+function findPeakElement(nums: number[]): number {
+  let result;
+  nums.forEach((item, index) => {
+    const prev = index > 0 ? nums[index - 1] : -Infinity;
+    const next = index < nums.length - 1 ? nums[index + 1] : -Infinity;
+    const isPeak = item > prev && item > next;
+    if (isPeak) {
+      result = index;
+      return false;
+    }
+  });
+  return result;
+}
