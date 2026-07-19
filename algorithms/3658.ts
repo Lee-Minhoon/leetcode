73d16b022bc357cdc624b84df70cf0f3c3c7c4fb
Lee Minhoon <hackids@naver.com>
2026-07-19 19:45:32 +0900

    solved algorithm 3658

---
+++ algorithms/3658.ts
@@ -0,0 +1,15 @@
+// 3658. GCD of Odd and Even Sums
+
+function getOddSums(n: number): number {
+  return n ** 2;
+}
+
+function getEvenSums(n: number): number {
+  return n ** 2 + n;
+}
+
+function gcdOfOddEvenSums(n: number): number {
+  return n;
+}
+
+console.log(gcdOfOddEvenSums(5));
